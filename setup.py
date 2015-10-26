f7117156b3a7fc1055af9fba621de2f428f82c2a
Brandon Dwiel <bhdwiel@ncsu.edu>
2015-10-26 19:09:42 -0400

    updated to version 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="halogen",
-    version="0.1.0",
+    version="1.2.0",
     packages=find_packages(),
     author="Brandon Dwiel",
     author_email="bhdwiel@ncsu.edu"
