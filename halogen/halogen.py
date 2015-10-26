d482a10975d5e47fccdd760c9a74935ac0f537b6
Brandon Dwiel <bhdwiel@ncsu.edu>
2015-10-26 19:16:39 -0400

    added some new, awesome feature

--- halogen/halogen.py
+++ halogen/halogen.py
@@ -7,40 +7,40 @@ import forms
 import db_utils
 
 def select_procs(stat_name, proc_names):
-    '''
-    turn a list of processor names into a list of names to access the stats_db with.
-    if <stat_name> is a core stat, turn each processor name into '<proc_name> <core_name>'
-    for each core in the processor.
-    if <stat_name> is a process stat, turn each processor name into '<proc_name> process_0'
-    '''
-    # find the type of stat
-    stat_type = stat_name.split('.')[0]
+  '''
+  turn a list of processor names into a list of names to access the stats_db with.
+  if <stat_name> is a core stat, turn each processor name into '<proc_name> <core_name>'
+  for each core in the processor. 
+  if <stat_name> is a process stat, turn each processor name into '<proc_name> process_0'
+  '''
+  # find the type of stat
+  stat_type = stat_name.split('.')[0]
 
-    # list of names to access the stats db with
-    db_names = []
+  # list of names to access the stats db with
+  db_names = []
 
-    if stat_type == 'processor':
-        return proc_names
-    elif stat_type == 'core':
-        for p in proc_names:
-            # find how many cores p has
-            row = db_utils.find_one('knobs', fields={'type': 'processor', 'name': p})
-            if row is None:
-                logging.error('problem finding {0} in the knobs db'.format(p))
-                continue
-            for core_name in row.knobs['core_names']:
-                # <processor_name> <core_name>
-                db_names.append(p + ' ' + core_name)
-        return db_names
-    elif stat_type == 'process':
-        # right now only 1 process per processor is supported
-        for p in proc_names:
-            # <processor_name> process_0
-            db_names.append(p + ' process_0')
-        return db_names
-    else:
-        logging.error('unknown stat type: {0}'.format(stat_type))
-        return proc_names
+  if stat_type == 'processor':
+    return proc_names
+  elif stat_type == 'core':
+      for p in proc_names:
+        # find how many cores p has
+        row = db_utils.find_one('knobs', fields={'type': 'processor', 'name': p})
+        if row is None:
+            logging.error('problem finding {0} in the knobs db'.format(p))
+            continue
+        for core_name in row.knobs['core_names']:
+            # <processor_name> <core_name>
+            db_names.append(p + ' ' + core_name)
+      return db_names
+  elif stat_type == 'process':
+      # right now only 1 process per processor is supported
+      for p in proc_names:
+        # <processor_name> process_0
+        db_names.append(p + ' process_0')
+      return db_names
+  else:
+      logging.error('unknown stat type: {0}'.format(stat_type))
+      return proc_names
 
 def select_benchmarks(names):
     '''
@@ -55,7 +55,7 @@ def select_benchmarks(names):
         # add all benchmarks from a suite
         if '.all' in name:
             # strip '.all' from the end of name
-            suite = name.rstrip('.all')
+             suite = name.rstrip('.all')
             rows = db_utils.find('benchmarks', fields={'suite': suite})
             # add benchmarks matching suite
             bm_list += [b.name for b in rows]
@@ -78,7 +78,7 @@ def process_chart_request(request):
     if request.args.get('normalize') == 'true':
         normalize = True
     else:
-        normalize = False
+         normalize = False
 
     if request.args.get('average') == 'true':
         average = True
@@ -90,13 +90,13 @@ def process_chart_request(request):
     else:
         hmean = False
 
-    logging.debug('statName: ' + stat_name)
-    logging.debug('benchmarks: ' + str(benchmarks))
-    logging.debug('procs: ' + str(procs))
-    logging.debug('normalize: ' + str(normalize))
-    logging.debug('average: ' + str(average))
-    logging.debug('hmean: ' + str(hmean))
-    logging.debug(request.args)
+        logging.debug('statName: ' + stat_name)
+        logging.debug('benchmarks: ' + str(benchmarks))
+        logging.debug('procs: ' + str(procs))
+        logging.debug('normalize: ' + str(normalize))
+        logging.debug('average: ' + str(average))
+        logging.debug('hmean: ' + str(hmean))
+        logging.debug(request.args)
 
     return (stat_name, benchmarks, procs, normalize, average, hmean)
 
@@ -105,10 +105,10 @@ def get_stat_choices():
     return a (nested) dict of stats: {display_name: submit_name}
     '''
     # use the last core and processor stats
-    core_stats    = db_utils.find('stats', fields={'type': 'core'})[0].stats
-    proc_stats    = db_utils.find('stats', fields={'type': 'processor'})[0].stats
-    process_stats = db_utils.find('stats', fields={'type': 'process'})[0].stats
-    stat_choices  = {'core': core_stats, 'processor': proc_stats, 'process': process_stats}
+        core_stats    = db_utils.find('stats', fields={'type': 'core'})[0].stats
+        proc_stats    = db_utils.find('stats', fields={'type': 'processor'})[0].stats
+        process_stats = db_utils.find('stats', fields={'type': 'process'})[0].stats
+        stat_choices  = {'core': core_stats, 'processor': proc_stats, 'process': process_stats}
 
     # replace the stat value with the submit_name (hierarchical name joined by '.')
     def replace_value(d, name=None):
